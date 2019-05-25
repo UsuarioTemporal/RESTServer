62f85d6a8ca0c9f904cf8ed69f327625449ef686
UsuarioTemporal <thomtwd@gmail.com>
2019-05-25 12:14:16 -0500

    Aclaracion rápida

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+
+/**
+ * Un esquema puede referirse a una reperesentación visual a una bd , a un
+ *  conjunto de reglas que rige una bd, o bien , a todo el conjunto de 
+ * objetos que pertenen a un usuario en particular.
+ * 
+ * Representa una configuración lógica de todo o parte de una db relacional.Puede existir de dos formas : como reperesentacion visual y como un conjunto de fórmulas conocidas como restricciones de integridad
+ */
+
 const express = require('express'),
     port = require('./config/config'),
     app = express()
