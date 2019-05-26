d6fcd2a72ec609fe32ded44acb57f3ae08f23b76
UsuarioTemporal <thomtwd@gmail.com>
2019-05-26 17:11:56 -0500

    Agregando plugin para la validacion de atributos repetidos

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose'),
+    uniqueValidator = require('mongoose-unique-validator')
 let Schema = mongoose.Schema
 let userSchema = new Schema({
     name:{
@@ -32,5 +33,6 @@ let userSchema = new Schema({
     }
 })
 
+userSchema.plugin(uniqueValidator,{message:'{PATH} debe de ser unico'})
 
 module.exports=mongoose.model('User',userSchema)
