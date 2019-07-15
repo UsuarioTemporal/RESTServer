acf0c8519490800a1eabdabea776669f99dd0ee3
MaurickThom <thomtwd@gmail.com>
2019-07-15 17:50:08 -0500

    [instalando la dependencia bcrypt]

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,16 +8,13 @@ app.get('/',(req,res)=>{
 })
 .post('/user',(req,res)=>{
     let body = req.body,
-        user = new User({
-            ...body
-        })
+        user = new User({...body})
+    
     user.save((err,userDB)=>{ // esto puede recibir un callback que es un error o un usuario de la base de datos que es una respuesta que es el usuario que se grabo en mongo
-        if(err){
-            return res.status(400).json({
-                ok:false,
-                err
-            })
-        }
+        if(err) return res.status(400).json({
+            ok:false,
+            err
+        })
             
         res.json({
             ok:true,
