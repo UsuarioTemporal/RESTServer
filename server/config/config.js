922979493ff8035caaf28561258626ac4da4f8ab
MaurickThom <thomtwd@gmail.com>
2019-07-17 01:03:36 -0500

    [url mongo atlas]

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB=' '
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/coffe'
 }else{
-    urlDB='mongodb://localhost:27017/coffe'
+    urlDB='mongodb+srv://thom:thom@cluster0-khfqk.mongodb.net/test?retryWrites=true&w=majority'
 }
 
 process.env.URLDB = urlDB
