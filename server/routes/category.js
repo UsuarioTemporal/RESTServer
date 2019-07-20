ded1711717fe290e90a8fc98055133034c405bd5
MaurickThom <thomtwd@gmail.com>
2019-07-19 19:38:56 -0500

    [se creo la ruta de categoria]

---
+++ server/routes/category.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+    {verifyToken} = require('../middlewares/authentication'),
+    app = express(),
+    Categoria= require('../models/category')
+
+//mostrar todas las categorias
+
+//mostrar una categoria por id
+
+//crear nueva categoria
+
+//actulizar la categoria
+
+//eliminar categoria
+
+
+module.exports = app
