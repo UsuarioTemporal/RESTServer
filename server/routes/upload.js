6f59fe62288ee40d18f3b3865d3fa930b5924238
MaurickThom <thomtwd@gmail.com>
2019-07-21 23:28:49 -0500

    [Creacion de carpetas específicas para usuario y producto]

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,36 +1,126 @@
 const express = require('express'),
     fileUpload = require('express-fileupload'),
-    app = express()
+    app = express(),
+    User = require('../models/user'),
+    Product = require('../models/product'),
+    fs = require('fs')
 
 app.use(fileUpload())
 
-app.put('/upload',(req,res)=>{
-    let files = req.files
+// funcion para la eliminacion de una imagen
+const deleteFile = (nameImg,type)=>{
+    let pathImg = `${__dirname}/../uploads/${type}/${nameImg}`
+    if(fs.existsSync(pathImg)) fs.unlinkSync(pathImg)
+}
+
+// insertar imagen a usuario
+const userImg = (id,res,fileName)=>{
+
+    User.findById(id,(err,userDB)=>{
+        if(err) {
+            deleteFile(fileName,'users')
+            return res.status(500).json({
+                ok:false,
+                err
+            })
+        }
+        if(!userDB) {
+            deleteFile(fileName,'users')
+            return res.status(400).json({
+                ok:false,
+                err:{
+                    message:'el usuario no existe'
+                }
+            })
+        }
+
+        deleteFile(userDB.img,'users')
+        userDB.img = fileName
+        userDB.save((err,savedUser)=>{
+            res.json({
+                ok:true,
+                user:savedUser,
+                img: fileName
+            })
+        })
+    })
+}
+
+//insertar imagen al producto
+const productImg = (id,res,fileName)=>{
+    Product.findById(id,(err,productDB)=>{
+        if(err) {
+            deleteFile(fileName,'products')
+            return res.status(500).json({
+                ok:false,
+                err
+            })
+        }
+
+        if(!productDB){
+            deleteFile(fileName,'products')
+            return res.status(400).json({
+                ok:false,
+                err:{
+                    message:'Usuario no existe'
+                }
+            })
+        }
+        deleteFile(productDB.img,'products')
+        productDB.img=fileName
+        productDB.save((err,savedProduct)=>{
+            res.json({
+                ok:true,
+                product:savedProduct,
+                img: fileName
+            })
+        })
+    })
+}
+
+app.put('/upload/:type/:id',(req,res)=>{
+    let files = req.files,
+        type = req.params.type,
+        id = req.params.id
     if(!files) return res.status(400).json({
         ok:false,
-        message:'No se ha seleccinado ningun archivo'
+        message:'No se ha seleccioado ningun archivo'
     })
-    let sampleFile = files.file // este file se lo pasaremos en el body
-    let nameFile = sampleFile.name.split('.').slice(-1)
-    // Extensiones permitidas
-    let validExtentions = ['png','jpg','gif','jpeg']
 
-    if(validExtentions.indexOf(nameFile)<0) return res.status(400).json({
+    // Validar el tipo
+    let validTypes = ['product','user']
+    if(validTypes.indexOf(type) < 0) return res.status(400).json({
         ok:false,
         err:{
-            message:'La extension no es correcta'
+            message:`Los tipos permitidos son ${validTypes.join(', ')}`,
+            type
         }
     })
 
-    sampleFile.mv('./uploads/filename.jpg',err=>{
+    let sampleFile = files.file // este file se lo pasaremos en el body
+    let nameFile = sampleFile.name
+    let extension = nameFile.split('.').slice(-1)
+    // Extensiones permitidas
+    let validExtentions = ['png','jpg','gif','jpeg']
+
+    if(validExtentions.indexOf(extension)<0) return res.status(400).json({
+        ok:false,
+        err:{
+            message:`La extension no es correcta , las extensiones permitidas son ${validExtentions.join(', ')}`,
+            extension
+        }
+    })
+
+    let nameRealFile = `${id}-${new Date().getMilliseconds()}.${extension}` 
+    sampleFile.mv(`./uploads/${type}/${nameRealFile}`,err=>{
         if(err) return res.status(500).json({
             ok:false,
             err
         })
-        return res.json({
-            ok:true,
-            message:'Imagen subida correctamente'
-        })
+
+        if(type==='users') return userImg(id,res,nameRealFile)
+
+        productImg(id,res,nameRealFile)
     })
 })
 
