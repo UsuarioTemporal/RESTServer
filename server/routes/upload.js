0d9a9a50655f162d38e9e7ca85a3da44559e540c
MaurickThom <thomtwd@gmail.com>
2019-07-21 21:32:55 -0500

    [validando las extensiones de los archivos]

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -11,6 +11,17 @@ app.put('/upload',(req,res)=>{
         message:'No se ha seleccinado ningun archivo'
     })
     let sampleFile = files.file // este file se lo pasaremos en el body
+    let nameFile = sampleFile.name.split('.').slice(-1)
+    // Extensiones permitidas
+    let validExtentions = ['png','jpg','gif','jpeg']
+
+    if(validExtentions.indexOf(nameFile)<0) return res.status(400).json({
+        ok:false,
+        err:{
+            message:'La extension no es correcta'
+        }
+    })
+
     sampleFile.mv('./uploads/filename.jpg',err=>{
         if(err) return res.status(500).json({
             ok:false,
